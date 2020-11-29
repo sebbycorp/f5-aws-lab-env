c99d6f589050d2fd69a41006d63f588bb31968d6
sebbycorp <sebastian@maniak.io>
2020-11-28 20:41:55 -0500

    update

---
+++ terraform/main.tf
@@ -0,0 +1,48 @@
+
+terraform {
+  backend "s3" {
+    bucket         = "f5-aws-lab-env"
+    key            = "f5-aws-lab-env.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "f5-aws-lab-env-state-lock"
+  }
+}
+
+resource "aws_s3_bucket" "f5-aws-lab-env-s3" {
+  bucket = "f5-aws-lab-env"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+}
+provider "aws" {
+  region = var.aws_region
+}
+
+module network {
+  source = "./modules/network"
+}
+
+module security {
+  source = "./modules/security"
+
+}
+
+module bigip {
+  source = "./modules/bigip"
+
+}
+
+module app {
+  source = "./modules/app"
+}
+
+module automation {
+  source = "./modules/automation"
+}
+
+module monitor {
+  source = "./modules/automation"
+}
